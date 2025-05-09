2d1251829395a99f91daeeef5a59e49f58ce7c3f
AlonKo68 <fraualenko@gmail.com>
2025-05-09 11:18:03 +0200

    create repo

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import css from './App.module.css'
+
+
+function App() {
+  return (
+    <>
+      <p className={css.read_the_docs}>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+export default App
